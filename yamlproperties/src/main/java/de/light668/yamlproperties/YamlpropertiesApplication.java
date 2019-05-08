f04d7cd3751ceb29bdf8e91fa8439af7bff50c88
unknown <timo.radtke@wwu.de>
2019-05-08 13:41:19 +0200

    added last subsection about yaml properties

---
+++ yamlproperties/src/main/java/de/light668/yamlproperties/YamlpropertiesApplication.java
@@ -0,0 +1,13 @@
+package de.light668.yamlproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamlpropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamlpropertiesApplication.class, args);
+	}
+
+}
